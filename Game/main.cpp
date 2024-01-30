41492106672bc2b5a5707b89c88ded7377c784f4
marwansakas <31859311+marwansakas@users.noreply.github.com>
2024-01-30 22:27:36 +0200

    finished assignment

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,33 +1,58 @@
+#include <glfw/deps/glad/gl.h>
 #include "InputManager.h"
 // #include "../DisplayGLFW/display.h"
 #include "game.h"
 #include "../res/includes/glm/glm.hpp"
+#include "stb_image.h";
 
-int main(int argc,char *argv[])
+
+
+int main(int argc, char* argv[])
 {
-	const int DISPLAY_WIDTH = 800;
-	const int DISPLAY_HEIGHT = 800;
+	fprintf(stderr, "test\n");
+	const int DISPLAY_WIDTH = 512;
+	const int DISPLAY_HEIGHT = 512;
 	const float CAMERA_ANGLE = 0.0f;
 	const float NEAR = 1.0f;
 	const float FAR = 100.0f;
 
-	Game *scn = new Game(CAMERA_ANGLE,(float)DISPLAY_WIDTH/DISPLAY_HEIGHT,NEAR,FAR);
-	
+	Game* scn = new Game(CAMERA_ANGLE, (float)DISPLAY_WIDTH / DISPLAY_HEIGHT, NEAR, FAR);
+
 	Display display(DISPLAY_WIDTH, DISPLAY_HEIGHT, "OpenGL");
-	
+
 	Init(display);
-	
+
 	scn->Init();
 
 	display.SetScene(scn);
 
-	while(!display.CloseWindow())
+	while (!display.CloseWindow())
 	{
-		scn->Draw(1,0,scn->BACK,true,false);
+
+		// GrayScale
+		scn->SetShapeTex(0, 0);
+		glViewport(0, DISPLAY_HEIGHT / 2, DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2);
+		scn->Draw(1, 0, scn->BACK, true, false);
+
+		// half tone
+		scn->SetShapeTex(0, 1);
+		glViewport(0, 0, DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2);
+		scn->Draw(1, 0, scn->BACK, false, false);
+
+		// floyd
+		scn->SetShapeTex(0, 2);
+		glViewport(DISPLAY_WIDTH / 2, 0, DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2);
+		scn->Draw(1, 0, scn->BACK, false, false);
+
+		// edges
+		scn->SetShapeTex(0, 3);
+		glViewport(DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2, DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2);
+		scn->Draw(1, 0, scn->BACK, false, false);
+
 		scn->Motion();
 		display.SwapBuffers();
-		display.PollEvents();	
-			
+		display.PollEvents();
+
 	}
 	delete scn;
 	return 0;
